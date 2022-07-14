9e4f4f3699833993ad54c0cdb15d67287eda4279
larissaaiala <larissaaialasilva@gmail.com>
2022-07-14 14:59:23 +0200

    [ISB-176] Correzione exception HpanList

--- src/main/java/it/gov/pagopa/payment/instrument/constants/PaymentInstrumentConstants.java
+++ src/main/java/it/gov/pagopa/payment/instrument/constants/PaymentInstrumentConstants.java
@@ -8,6 +8,8 @@ public final class PaymentInstrumentConstants {
   public static final String ERROR_PAYMENT_INSTRUMENT_NOT_FOUND = "The selected payment instrument is not active for such user and initiative.";
   public static final String ERROR_PAYMENT_INSTRUMENT_ALREADY_ACTIVE = "Payment instrument already in use by another citizen";
 
+  public static final String ERROR_INITIATIVE_USER = "It doesn't exist payment instrument for such initiative and user";
+
   private PaymentInstrumentConstants() {
   }
 }
