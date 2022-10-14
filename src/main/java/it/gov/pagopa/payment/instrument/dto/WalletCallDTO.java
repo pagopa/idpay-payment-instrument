d3511ac0d1658681e6d9d9662678ab0df250ed77
larissaaiala <larissaaialasilva@gmail.com>
2022-10-14 10:09:48 +0200

    [ISB-196] Sviluppo API lista carte (correzioni)

--- src/main/java/it/gov/pagopa/payment/instrument/dto/WalletCallDTO.java
+++ src/main/java/it/gov/pagopa/payment/instrument/dto/WalletCallDTO.java
@@ -2,12 +2,12 @@ package it.gov.pagopa.payment.instrument.dto;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
+@Data
 public class WalletCallDTO {
 
   private List<WalletDTO> walletDTOlist;
