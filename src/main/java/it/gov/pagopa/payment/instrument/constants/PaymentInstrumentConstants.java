1941e74f1cdc9221f7b164edf0c4a2f239767320
larissaaiala <larissaaialasilva@gmail.com>
2022-10-25 17:22:47 +0200

    PaymentInstrumentConstants

--- src/main/java/it/gov/pagopa/payment/instrument/constants/PaymentInstrumentConstants.java
+++ src/main/java/it/gov/pagopa/payment/instrument/constants/PaymentInstrumentConstants.java
@@ -25,8 +25,8 @@ public final class PaymentInstrumentConstants {
   public static final String ERROR_RTD = "error to ADD new instrument to RTD";
   public static final String PM="PAYMENT_MANAGER";
   public static final String IO="APP_IO";
-  public static final String SATISPAY = "SATISPAY";
-  public static final String BPAY = "BPAY";
+  public static final String SATISPAY = "Satispay";
+  public static final String BPAY = "BPay";
   public static final String STATUS_PENDING_ENROLLMENT_REQUEST = "PENDING_ENROLLMENT_REQUEST";
   public static final String STATUS_PENDING_DEACTIVATION_REQUEST = "PENDING_DEACTIVATION_REQUEST";
   public static final String STATUS_FAILED_ENROLLMENT_REQUEST = "INACTIVE_FAILED_ENROLLMENT_REQUEST";
