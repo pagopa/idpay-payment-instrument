622b57fe02f7be0432258a341e404c6ed6891cef
Marco Di Lisa <marco.di.lisa@nttdata.com>
2022-12-06 14:26:17 +0100

    [ISB-619] Added enrollFromIssuer

--- src/main/java/it/gov/pagopa/payment/instrument/dto/HpanDTO.java
+++ src/main/java/it/gov/pagopa/payment/instrument/dto/HpanDTO.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.payment.instrument.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(Include.NON_NULL)
 public class HpanDTO {
 
   private String maskedPan;
