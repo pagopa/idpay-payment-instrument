2449cb44262c5047440305c1bb02ad13450db888
larissaaiala <larissaaiala.silvaleite.st@nttdata.com>
2023-09-27 09:50:14 +0200

    IDP-1796-sviluppo-api-get-status-idpay-add fix import test

--- src/test/java/it/gov/pagopa/payment/instrument/service/PaymentInstrumentServiceTest.java
+++ src/test/java/it/gov/pagopa/payment/instrument/service/PaymentInstrumentServiceTest.java
@@ -61,6 +61,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
