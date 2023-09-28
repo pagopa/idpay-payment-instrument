e90e5561b551e7c42b8fd8037f35c1084942cbd0
larissaaiala <larissaaiala.silvaleite.st@nttdata.com>
2023-09-28 15:26:08 +0200

    fix-response-from-wallet

--- src/main/java/it/gov/pagopa/payment/instrument/dto/CheckEnrollmentDTO.java
+++ src/main/java/it/gov/pagopa/payment/instrument/dto/CheckEnrollmentDTO.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.payment.instrument.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CheckEnrollmentDTO {
-  @JsonProperty("isIdPayCodeEnabled")
-  private boolean isIdPayCodeEnabled;
+
+  private Boolean isIdPayCodeEnabled;
 
 }
