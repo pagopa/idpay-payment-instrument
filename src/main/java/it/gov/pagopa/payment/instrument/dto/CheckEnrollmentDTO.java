7bac1134fb4ef85be4145805137636c8adcad5ce
frtrinca <francesco.trinca@nttdata.com>
2023-09-28 15:08:05 +0200

    fix dto

--- src/main/java/it/gov/pagopa/payment/instrument/dto/CheckEnrollmentDTO.java
+++ src/main/java/it/gov/pagopa/payment/instrument/dto/CheckEnrollmentDTO.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.payment.instrument.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CheckEnrollmentDTO {
-
+  @JsonProperty("isIdPayCodeEnabled")
   private boolean isIdPayCodeEnabled;
 
 }
