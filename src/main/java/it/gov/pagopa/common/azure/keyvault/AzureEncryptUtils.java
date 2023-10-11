ce7c5cc9c14dbc44e88e144fb4575a6c529cd6c5
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2023-10-11 19:17:50 +0200

    feat: IDP-1935 KeyVaultPOC (#156)
    * IDP-1935 KeyVaultPOC
    
    * ignoring netty known false-positive CVE
    
    * Algorithm not more defined as default

---
+++ src/main/java/it/gov/pagopa/common/azure/keyvault/AzureEncryptUtils.java
@@ -0,0 +1,49 @@
+package it.gov.pagopa.common.azure.keyvault;
+
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.security.keyvault.keys.KeyClient;
+import com.azure.security.keyvault.keys.KeyClientBuilder;
+import com.azure.security.keyvault.keys.cryptography.CryptographyClient;
+import com.azure.security.keyvault.keys.cryptography.CryptographyClientBuilder;
+import com.azure.security.keyvault.keys.cryptography.models.EncryptionAlgorithm;
+import com.azure.security.keyvault.keys.models.KeyVaultKey;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class AzureEncryptUtils {
+
+    private static final DefaultAzureCredential DEFAULT_AZURE_CREDENTIAL = new DefaultAzureCredentialBuilder().build();
+
+    private AzureEncryptUtils(){}
+
+    public static KeyClient getKeyClient(String keyVaultUrl){
+        return new KeyClientBuilder()
+                .vaultUrl(keyVaultUrl)
+                .credential(DEFAULT_AZURE_CREDENTIAL)
+                .buildClient();
+    }
+
+    public static CryptographyClient buildCryptographyClient(KeyVaultKey key){
+        return new CryptographyClientBuilder()
+                .keyIdentifier(key.getId())
+                .credential(DEFAULT_AZURE_CREDENTIAL)
+                .buildClient();
+    }
+
+    public static String encrypt(String plainValue, EncryptionAlgorithm encryptionAlgorithm, CryptographyClient cryptoClient) {
+        // String -> byte[] -> RSA -> Base64
+        return Base64.getEncoder()
+                .encodeToString(
+                        cryptoClient.encrypt(encryptionAlgorithm,
+                                plainValue.getBytes(StandardCharsets.UTF_8)).getCipherText());
+    }
+
+    public static String decrypt(String encryptedValue, EncryptionAlgorithm encryptionAlgorithm, CryptographyClient cryptoClient) {
+        // Base64 -> RSA -> byte[] -> String
+        return new String(
+                cryptoClient.decrypt(encryptionAlgorithm,
+                        Base64.getDecoder().decode(encryptedValue)).getPlainText(), StandardCharsets.UTF_8);
+    }
+}
---
+++ src/test/java/it/gov/pagopa/common/azure/keyvault/AzureEncryptUtilsTest.java
@@ -0,0 +1,70 @@
+package it.gov.pagopa.common.azure.keyvault;
+
+import com.azure.security.keyvault.keys.KeyClient;
+import com.azure.security.keyvault.keys.cryptography.CryptographyClient;
+import com.azure.security.keyvault.keys.cryptography.models.DecryptResult;
+import com.azure.security.keyvault.keys.cryptography.models.EncryptResult;
+import com.azure.security.keyvault.keys.cryptography.models.EncryptionAlgorithm;
+import com.azure.security.keyvault.keys.models.KeyVaultKey;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
+import org.mockito.Mockito;
+
+import java.nio.charset.StandardCharsets;
+
+class AzureEncryptUtilsTest {
+
+    public static final EncryptionAlgorithm ENCRYPTION_ALGORITHM = EncryptionAlgorithm.RSA_OAEP;
+
+    @Test
+    void testGetKeyClient(){
+        // Given
+        String keyVaultUrl = "https://KEYVAULTNAME.vault.azure.net";
+
+        // When
+        KeyClient keyClient = AzureEncryptUtils.getKeyClient(keyVaultUrl);
+
+        // Then
+        Assertions.assertNotNull(keyClient);
+        Assertions.assertEquals(keyVaultUrl, keyClient.getVaultUrl());
+    }
+
+    @Test
+    void testBuildCryptographyClient() throws Exception {
+        // Given
+        String expectedKeyId = "https://KEYVAULTNAME.vault.azure.net/keys/KEYNAME/KEYID";
+
+        KeyVaultKey keyVaultKeyMock = Mockito.mock(KeyVaultKey.class);
+        Mockito.when(keyVaultKeyMock.getId()).thenReturn(expectedKeyId);
+
+        // When
+        CryptographyClient cryptographyClient = AzureEncryptUtils.buildCryptographyClient(keyVaultKeyMock);
+
+        // Then
+        Assertions.assertNotNull(cryptographyClient);
+        Assertions.assertSame(expectedKeyId, ReflectionUtils.tryToReadFieldValue(CryptographyClient.class, "keyId", cryptographyClient).get());
+    }
+
+    @Test
+    void testEncryptDecrypt(){
+        // Given
+        String keyId = "KEYID";
+        String plainvalue = "PLAINVALUE";
+        byte[] cipherValue = "CIPHERVALUE".getBytes(StandardCharsets.UTF_8);
+
+        CryptographyClient cryptographyClientMock = Mockito.mock(CryptographyClient.class);
+        Mockito.when(cryptographyClientMock.encrypt(ENCRYPTION_ALGORITHM, plainvalue.getBytes(StandardCharsets.UTF_8)))
+                .thenReturn(new EncryptResult(cipherValue, ENCRYPTION_ALGORITHM, keyId));
+        Mockito.when(cryptographyClientMock.decrypt(ENCRYPTION_ALGORITHM, cipherValue))
+                .thenReturn(new DecryptResult(plainvalue.getBytes(StandardCharsets.UTF_8), ENCRYPTION_ALGORITHM, keyId));
+
+        // When
+        String encryptedValue = AzureEncryptUtils.encrypt(plainvalue, ENCRYPTION_ALGORITHM, cryptographyClientMock);
+        Assertions.assertNotNull(encryptedValue);
+        String decryptedValue = AzureEncryptUtils.decrypt(encryptedValue, ENCRYPTION_ALGORITHM, cryptographyClientMock);
+
+        // Then
+        Assertions.assertEquals(plainvalue, decryptedValue);
+    }
+}
