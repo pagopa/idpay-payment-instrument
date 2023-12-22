0fd85a2987d34825807fe65762be5b0e3886b7ca
stefanodel <stefano11.delia@nttdata.com>
2023-12-22 14:35:09 +0100

    paymentInstrument handle service exception error

--- src/test/java/it/gov/pagopa/common/web/exception/ErrorManagerTest.java
+++ src/test/java/it/gov/pagopa/common/web/exception/ErrorManagerTest.java
@@ -59,12 +59,14 @@ class ErrorManagerTest {
     memoryAppender.setContext((LoggerContext) LoggerFactory.getILoggerFactory());
     memoryAppender.start();
   }
+
   @BeforeEach
-  void clearMemoryAppender(){
+  void clearAndAppendMemoryAppender(){
+    memoryAppender.reset();
+
     ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(ErrorManager.class.getName());
     logger.setLevel(ch.qos.logback.classic.Level.INFO);
     logger.addAppender(memoryAppender);
-    memoryAppender.reset();
   }
 
   @Test
