abff58d84768147dd054d07a423603e3257b35c5
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2024-01-11 18:05:05 +0100

    fix: IDP-2196 fix Cve (#187)

--- src/test/java/it/gov/pagopa/common/azure/keyvault/AzureEncryptUtilsTestIntegrated.java
+++ src/test/java/it/gov/pagopa/common/azure/keyvault/AzureEncryptUtilsTestIntegrated.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
- * In order to be allowed to execute this test, you must have AZ cli installed and logged into CSTAR-DEV subscription
+ * In order to be allowed to execute this test, you must have AZ cli installed and logged into DEV-CSTAR subscription.<br />
+ * Run the following command:
+ * <pre>{@code az login --scope https://vault.azure.net/.default}</pre>
  */
 @SuppressWarnings({"squid:S3577", "NewClassNamingConvention"}) // suppressing class name not match alert: we are not using the Test suffix in order to let not execute this test by default maven configuration because it depends on properties not pushable. See
 class AzureEncryptUtilsTestIntegrated {
